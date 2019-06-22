c11c306ec89cbcf1512da4bd9cc67664396daca8
rintukutum <rintukutum@gmail.com>
2019-06-22 12:13:59 +0530

    template for jpost

---
+++ 00-selenium-app-jpost.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+#------------
+
+#------------
+driver = webdriver.Firefox()
+for page in range(1,15):
+    if page == 1:
+        driver.get('https://repository.jpostdb.org')
+    else:
+        driver.find_element_by_xpath("//a[@data-page='2']").click()
+    page_source = driver.page_source
+	page_content = html.document_fromstring(page_source)
