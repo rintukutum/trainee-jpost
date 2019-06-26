089619408fe333156d2b81e6552db112b8ff8b55
aagammishra27 <aagam.mishra.27@gmail.com>
2019-06-26 21:19:34 +0530

    updated

--- 03-convert-xml-to-metadata.py
+++ 03-convert-xml-to-metadata.py
@@ -30,8 +30,48 @@ def extract_id_value_with_tagName(tagName,doc):
     tagName_out = {'id': tagName_id, 'value': tagName_val}
     return tagName_out
 
-extract_id_value_with_tagName('Species',doc)
+def extract_id_value_with_Species(tagName,doc):
+     tagName = 'Species'
+     y= doc.getElementsByTagName(tagName)[0].childNodes[1].childNodes
+     tagName_id=y.attributes.get('id').value
+     tagName_val=y.attributes.get('value').value
+     tagName_out={'id': tagName_id, 'value': tagName_val}
+     return tagName_out
+    
+    
 
 # not working for other tags 
 # fix it
-extract_id_value_with_tagName('Instrument',doc)
+def extract_id_value_with_Instrument(tagName,doc):
+    tagName = 'Instrument'
+    y=doc.getElementsByTagName(tagName)[0].childNodes[1].childNodes
+    tagName_id=y.attributes.get('id').value
+    tagName_val=y.attributes.get('value').value
+    tagName_out={'id': tagName_id, 'value': tagName_val}
+    return tagName_out
+"""
+def extract_id_value_with_Modification(tagName,doc):
+    tagName= 'Modification'
+    ele=[]
+    for num in range(0,8):
+        if num % 2 != 0:
+            ele.append(doc.getElementsByTagName(tagName)[0].childNodes[num].childNodes)
+            tagName_id=ele.attributes.get('id').value
+            tagName_val=ele.attributes.get('value').value
+            tagName_out={'id': tagName_id, 'value': tagName_val}
+    return tagName_out
+"""            
+def extract_id_value_with_FileList(tagName,doc):
+    tagName= 'FileList'
+    el=[]
+    for n in range(0,18):
+        if n % 2!=0:
+            el.append(doc.getElementsByTagName(tagName)[0].childNodes[n].childNodes)
+            tagName_id=el.attributes.get('id').value
+            tagName_out={'id': tagName_id}
+    return tagName_out
+    
+    
+
+# Modification
+# FileList
