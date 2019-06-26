92dfb9defb82c4d0cd817ad6ea5ea91a05175619
aagammishra27 <51317812+aagammishra27@users.noreply.github.com>
2019-06-26 09:01:25 +0530

    Update project-details.py

--- project-details.py
+++ project-details.py
@@ -4,4 +4,23 @@ Created on Mon Jun 24 22:08:03 2019
 
 @author: Aaga
 """
+import urllib 
+import pickle
 
+project = open('hrefs-to-project-details.pickle', 'rb')
+project_data = pickle.load(project)
+project.close()
+
+import os
+#os.mkdir('./xml')
+i = 1
+for p in project_data:        
+    file=urllib.request.urlopen('https://repository.jpostdb.org' + p)
+    data=file.read()
+    file.close()
+    output =  '.' + p
+    print(i,'=',output)
+    i = i + 1
+    filehandle = open(output,'wb')
+    filehandle.write(data)
+    filehandle.close()
