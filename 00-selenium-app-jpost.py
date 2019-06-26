5ad0aac3efd649d6e6c18c0a938ca5eb13377e49
rintukutum <rintukutum@gmail.com>
2019-06-26 11:35:35 +0530

    made changes

--- 00-selenium-app-jpost.py
+++ 00-selenium-app-jpost.py
@@ -1,13 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 #------------
-
+#
 #------------
 driver = webdriver.Firefox()
+
+from lxml import html
 for page in range(1,15):
     if page == 1:
         driver.get('https://repository.jpostdb.org')
     else:
-        driver.find_element_by_xpath("//a[@data-page='2']").click()
+        driver.find_element_by_xpath("//a[@data-page=%d]" %page).click()
     page_source = driver.page_source
 	page_content = html.document_fromstring(page_source)
